5b21b072b1e0c22c37a29c9d404f351a70aae0d6
Luis <luis_8.51@hotmail.com>
2022-06-24 14:47:28 -0300

    Banco de Dados

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE CLIENTE(
+id INTEGER not null primary key autoincrement,
+nome varchar(80) not null,
+email varchar(40) not null,
+telefone INT
+);
+CREATE TABLE sqlite_sequence(name,seq);
